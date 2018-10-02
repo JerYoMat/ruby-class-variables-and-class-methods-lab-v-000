d6d7c77af85d053e3ca680b42693e765d928fffa
Jeremy Emata <jeremy.emata@gmail.com>
2018-10-02 23:48:56 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,12 +3,15 @@ class Song
   attr_accessor :name, :artist, :genre 
   
   @@song_count = 0 
+  @@artists = []
   
   def initialize(name, artist, genre)
     @name = name 
     @artist = artist 
     @genre = genre 
-    @@count += 1 
+    @@count += 1
+    @@artists << artist 
+    @@artists.uniq!
   end 
   
   def self.count 
