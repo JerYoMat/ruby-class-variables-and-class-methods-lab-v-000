3e4d89d1c642d8de1c43923b614560e65e88462d
Jeremy Emata <jeremy.emata@gmail.com>
2018-10-02 23:47:55 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,6 +4,15 @@ class Song
   
   @@song_count = 0 
   
-
+  def initialize(name, artist, genre)
+    @name = name 
+    @artist = artist 
+    @genre = genre 
+    @@song_count += 1 
+  end 
+  
+  def self.count 
+    @@song_count
+  end 
 
 end
